2d3d29bf048e2d4a6cd15e7e386352b46860edb3
999666-zk <15539234100zk@gmail.com>
2022-04-18 11:51:34 +0800

    april commit regist.html

---
+++ js/regist.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  var send_yan = document.getElementsByClassName("send_yan")[0];
+  var timer_yan = null;
+  var flag = true;
+  var num = 60;
+  send_yan.addEventListener("click", function () {
+    if (flag) {
+      timer_yan = setInterval(function () {
+        flag = false;
+        num--;
+        send_yan.innerHTML = num;
+        if (num <= 0) {
+          clearInterval(timer_yan);
+          flag = true;
+          num = 60;
+          send_yan.innerHTML = "重新发送";
+        }
+      }, 1000);
+    }
+  });
+};
