66e2201c583b7fb439282613e5b922f1557afab2
999666-zk <15539234100zk@gmail.com>
2022-04-21 10:58:03 +0800

    april will commit sp html

---
+++ js/view_how.js
@@ -0,0 +1,9 @@
+// window.onload = function () {
+//   function setRem() {
+//     var uiw = 414;
+//     var winw = document.documentElement.clientWidth;
+//     document.documentElement.style.fontSize = (winw / uiw) * 100 + "px";
+//   }
+//   setRem();
+//   window.onresize = setRem;
+// };
